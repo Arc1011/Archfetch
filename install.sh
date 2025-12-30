12b38f7c3f93b55b0ba8ce6c36480899cb6a9eb3
Arc1011 <pawelryszkowski1202@gmail.com>
2025-12-30 13:48:18 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Szybki instalator Archfetch
+python -m venv build_env
+source build_env/bin/activate
+pip install psutil pyinstaller
+pyinstaller --onefile --name archfetch archfetch.py
+sudo cp dist/archfetch /usr/local/bin/
+sudo chmod +x /usr/local/bin/archfetch
+echo "Instalacja zakończona! Wpisz 'archfetch', aby uruchomić."
