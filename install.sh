59ac8d0f4bf17c1e07731e8b2a9f426a315af90b
Arc1011 <pawelryszkowski1202@gmail.com>
2026-01-18 13:28:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ pip install psutil pyinstaller
 pyinstaller --onefile --name archfetch archfetch.py
 sudo cp dist/archfetch /usr/local/bin/
 sudo chmod +x /usr/local/bin/archfetch
+cd ~
 rm -rf Archfetch 
 echo "Instalation Complited! write "archfetch" to start"
